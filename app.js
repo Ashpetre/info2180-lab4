20eb7919b75291d8136ecab9b7ffd1c6126d94b5
ashli-ann petrekin <ashpetrekin@gmail.com>
2022-11-07 19:28:24 -0500

    completed lab

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+window.onload = function(){
+    const btn = document.getElementById("btn");
+    const search = document.getElementById("superhero");
+    var httpRequest;
+    var result = document.getElementById("result");
 
+    btn.addEventListener("click",function(element){
+        element.preventDefault();
+        httpRequest = new XMLHttpRequest();
+        var url = "superheroes.php?superhero="+ search.value;
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE){
+                if (httpRequest.status === 200){
+                    var response = httpRequest.responseText;
+                    result.innerHTML = response;
+                }
+                else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        };
+
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    })
+}
